05fa9da2c461d66425e6022c3eeec20ed455612b
Robinson Presotto <robipresotto@gmail.com>
2020-10-08 13:38:20 +0200

    Fix interpolation expression

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,12 +10,12 @@ terraform {
 variable "app" {}
 
 resource "docker_image" "app_api" {
-  name = "${var.app}"
+  name = var.app
 }
 
 resource "docker_container" "app_container" {
   image = docker_image.app_api.latest
-  name = "${var.app}"
+  name = var.app
 
   ports {
     internal = 8080
